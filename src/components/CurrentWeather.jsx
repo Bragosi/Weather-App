aefab6e052153f3bf012ef2e712d74b564543d00
Bragosi <oladejoboluwatife20003@gmail.com>
2025-06-07 19:36:23 +0100

    styling

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -2,12 +2,40 @@ import day1 from '../images/icons/01d.png'
 const CurrentWeather = () => {
   return (
    <div className="pl-[20px] pr-[20px] pb-[20px] w-[300px] border border-6 border-black shadow-xl bg-gray-800 ml-auto mr-auto mt-[15px]  ">
-        <div className="top flex justify-between items-center " >
+        <div className="flex justify-between items-center " >
           <div>
-            <p className="text-white font-bold text-[18px] m-0">Lagos</p>
-            <p className="font-medium text-[14px] mt-0 text-white "> Sunny </p>
+            <p className="text-white font-bold text-[18px]">Lagos</p>
+            <p className="font-medium text-[14px] mt-[-9px] text-white "> Sunny </p>
           </div>
             <img src={day1} alt="weather icon" width={100} />
+        </div>
+        <div className='bottom '>
+          <p className='temperature '> 18°C </p>
+        <div className='details'>
+           <div className='parameter-row '>
+            <span className='parameter-label'>Details </span>
+           </div>
+             <div className='parameter-row'>
+            <span className='parameter-label'>Feels like</span>
+            <span className='parameter-value'> 22°C</span>
+          </div>
+
+           <div className='parameter-row'>
+            <span className='parameter-label'>Wind</span>
+            <span className='parameter-value'>2 m/s</span>
+          </div>
+
+           <div className='parameter-row'>
+            <span className='parameter-label'>Humidity</span>
+            <span className='parameter-value'>15%</span>
+          </div>
+
+          <div className='parameter-row'>
+            <span className='parameter-label'>Pressure</span>
+            <span className='parameter-value'>15 hPa</span>
+          </div>
+        </div>
+
         </div>
    </div>
   )
